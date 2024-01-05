3e076de10df8e96889193431e82be3f3bad348f1
Babar <babarshahzad0@gmail.com>
2024-01-05 00:29:44 +0000

    Send data to API for storage

--- src/server/light_sensor.py
+++ src/server/light_sensor.py
@@ -5,7 +5,7 @@ def process_light_sensor(light_data, time):
     
     cnfg = config.getconfig()
     #construct api url with query string parameters
-    url = construct_api_url(cnfg, time)
+    url = construct_api_url(cnfg, time, light_data)
     #get the result from api either as true or false
     #depending on schedule
     result = api_handler.get_api_data(url)  
@@ -22,10 +22,10 @@ def process_light_sensor(light_data, time):
     else:
         return False
 
-def construct_api_url(config, time):
+def construct_api_url(config, time, data):
     apir_url = config.get("server", "api_url")
     sensor_id = config.get("server", "light_sensor_id")
     
-    url = "".join([apir_url, sensor_id, '/', time])
+    url = "".join([apir_url, sensor_id, '/', time, '/', data])
     
     return url
--- src/server/temp_sensor.py
+++ src/server/temp_sensor.py
@@ -3,13 +3,12 @@ import api_handler
 
 def process_temp_sensor(temprature_data, time):
     
+    temprature = float(temprature_data)
     #construct api url with query string parameters
     url = construct_api_url(time)
     #get the result from api either as true or false
     #depending on schedule
-    result = api_handler.get_api_data(url)   
-         
-    temprature = float(temprature_data)
+    result = api_handler.get_api_data(url, temprature)   
     
     #if temprature is 25C or more turn on the fan and return the status True.
     if result == True:
@@ -23,11 +22,11 @@ def process_temp_sensor(temprature_data, time):
         return False
     
     
-def construct_api_url(time):
+def construct_api_url(time, data):
     cnfg = config.getconfig()
     apir_url = cnfg.get("server", "api_url")
     sensor_id = cnfg.get("server", "temp_sensor_id")
     
-    url = "".join([apir_url, sensor_id, '/', time])
+    url = "".join([apir_url, sensor_id, '/', time, '/', data])
     
     return url
